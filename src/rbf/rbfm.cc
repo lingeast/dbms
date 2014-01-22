2b220d794afc2933c5aa7fe31f59ac808b1210f1
Yidong Ling <yidongl1@uci.edu>
2014-01-22 03:09:52 -0800

    Add comment to define skeleton of RecordBasedFileManager

--- src/rbf/rbfm.cc
+++ src/rbf/rbfm.cc
@@ -37,10 +37,44 @@ RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
 }
 
 RC RecordBasedFileManager::insertRecord(const FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
+	/*
+	 * //Transform raw record into variable length record format
+	 * DataBuffer buffer(recordDescriptor, data);
+	 * // Databuffer is a class that create & store record format data
+	 *
+	 *
+	 *	// Find a Page for insertion
+	 * rid.pageNum = fileHandle.findWritablePage();
+	 *
+	 *	// Read Page
+	 * PageHandle pg(pageNum, fileHandle);
+	 *	// Read Dir
+	 * PageDirHandle pdh(pageNum, fileHandle);
+	 *
+	 *	// Insert into page
+	 * rid.slotNum = pg.insertRecord(buffer.data(), buffer.length());
+	 *
+	 *	// Write back page
+	 * fileHandle.writePage(pg.pageID, pg.dataBlock());
+	 *	// Write back dir
+	 * fileHandle.writeDirBlock(dirCnt() * (sizeof(pageDir) + PAGE_DIR_SIZE * PAGE_SIZE, pdh);
+	 *
+	 */
     return -1;
 }
 
 RC RecordBasedFileManager::readRecord(const FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
+	/*
+	 *	PageHandle pg(rid.pageNum, fileHandle);
+	 *
+	 *	char * buffer;
+	 *
+	 *	pg.readRecord(rid.slotNum, buffer);
+	 *
+	 *	TransformBack(buffer, data);
+	 *
+	 *	delete buffer;
+	 */
     return -1;
 }
 
